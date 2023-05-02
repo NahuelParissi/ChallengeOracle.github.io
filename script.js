a182297a827e93a67056706f7f651864cd936b4e
NahuelP <nahuelparissi@gmail.com>
2023-05-01 21:05:26 -0300

    Agregado de funcionalidades

--- script.js
+++ script.js
@@ -36,24 +36,20 @@ const salida = document.querySelector('.salida')
 const textInput = document.querySelector('.text-input')
 const titulo = document.querySelector('.contenido-select')
 const p = document.querySelector('.p')
-const p2 = document.querySelector(".text-s p")
-const p3 = document.querySelector(".text-s h2")
-const p4 = document.querySelector('#text-out')
+const tEncripted = document.getElementById("textEncripted")
 const img1 = document.getElementById('img1')
 const img2 = document.createElement('img')
-
 
 document.getElementById("dark-mode").addEventListener("change", function () {
 	if (this.checked) {
 		document.body.style.backgroundColor = "#242424";
 		salida.style.setProperty("background", "#505050")
 		textInput.style.setProperty("background", "#242424")
+		textInput.style.setProperty("color", "white")
 		titulo.style.setProperty("color", "white")
 		opciones.style.setProperty("background", "#242424")
 		p.style.color = 'white';
-		p2.style.color = 'white';
-		p3.style.color = 'white';
-		p4.style.color = 'white';
+		tEncripted.style.color = 'white';
 		img2.src = 'img/!2.png';
 		img1.parentNode.replaceChild(img2, img1)
 
@@ -61,42 +57,67 @@ document.getElementById("dark-mode").addEventListener("change", function () {
 		document.body.style.backgroundColor = "";
 		salida.style.setProperty("background", "")
 		textInput.style.setProperty("background", "")
+		textInput.style.setProperty("color", "")
 		titulo.style.setProperty("color", "")
 		opciones.style.setProperty("background", "")
 		p.style.color = "#242424";
-		p2.style.color = "#242424";
-		p3.style.color = "#242424";
-		p4.style.color = "#242424"
+		tEncripted.style.color = "#242424";
 		img2.parentNode.replaceChild(img1, img2)
 	}
 });
 
-//encript pt1
+// Encript/Decrypt
+// Objeto que contiene las correspondencias entre las letras y sus valores encriptados
+const encryptionKey = {
+	"e": "enter",
+	"i": "imes",
+	"a": "ai",
+	"o": "ober",
+	"u": "ufat"
+};
 
-const textN = document.querySelector('.n-text');
-const textC = document.querySelector('.c-text');
-
-function alternar() {
-	if (textN.style.display === "flex") {
-		textN.style.display = "none";
-		textC.style.display = "flex";
-	} else {
-		textN.style.display = "flex";
-		textC.style.display = "none";
+function encrypt() {
+	const originalText = document.getElementById("textOriginal").value;
+	let encryptedText = "";
+	for (let i = 0; i < originalText.length; i++) {
+		const letter = originalText[i];
+		encryptedText += encryptionKey[letter] || letter;
 	}
+	document.getElementById("textEncripted").value = encryptedText;
+}
+
+const imagen = document.getElementById("imagen");
+const encryptButton = document.getElementById("bttn-E");
+const text = document.getElementById("textEncripted")
+
+encryptButton.addEventListener("click", function () {
+	imagen.style.display = "none";
+	text.style.height = "550px";
+});
+
+
+
+function decrypt() {
+	const encryptedText = document.getElementById("textOriginal").value;
+	let originalText = "";
+	for (let i = 0; i < encryptedText.length; i++) {
+		const letter = encryptedText.slice(i, i + 5);
+		if (encryptionKey[letter]) {
+			originalText += Object.keys(encryptionKey).find(key => encryptionKey[key] === letter);
+			i += 4;
+		} else {
+			originalText += encryptedText[i];
+		}
+	}
+	document.getElementById("textEncripted").value = encryptedText;
 }
 
 
 //boton copiar//
 
 function copiarTexto() {
-    var texto = document.getElementById('text-out');
-    var tempTextArea = document.createElement('textarea');
-    tempTextArea.value = texto.innerText;
-    document.body.appendChild(tempTextArea);
-    tempTextArea.select();
-    tempTextArea.setSelectionRange(0, 99999);
-    document.execCommand('copy');
-    document.body.removeChild(tempTextArea);
-    alert('Texto copiado: ' + texto.innerText);
+	const encryptedText = document.getElementById("textEncripted");
+	encryptedText.select();
+	document.execCommand("copy");
+	alert('Texto copiado');
 }
